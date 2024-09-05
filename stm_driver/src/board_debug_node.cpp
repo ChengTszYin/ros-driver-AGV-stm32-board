ef02e5a4ace3d6a5174fb4c6f2da0f52c84268f1
ChengTszYin <ken1999429@gmail.com>
2024-09-06 02:13:50 +0800

    txrx

--- stm_driver/src/board_debug_node.cpp
+++ stm_driver/src/board_debug_node.cpp
@@ -26,12 +26,26 @@ int16_t sensor2 = 0;
 int16_t sensor3 = 0;
 int16_t sensor4 = 0;
 
+int16_t lspeed = 0;
+int16_t rspeed = 0;
+
 
 int serial_read(serial::Serial &ser, std::string &result)
 {
     result = ser.read( ser.available() );
     return 0;
 }
+
+uint8_t getcrc(uint8_t* Bytecode, int len)
+{
+    uint8_t sum = 0;
+    for(int i=0; i<len; i++)
+    {
+        sum += Bytecode[i];
+    }
+    return sum;
+}
+
 
 
 int main(int argc, char** argv)
@@ -78,6 +92,18 @@ int main(int argc, char** argv)
 
     while( ros::ok() )
     {
+        uint8_t sendByte[8];
+        sendByte[0] = 0x00;
+        sendByte[1] = 0x01;
+        sendByte[2] = (lspeed >> 8) & 0xFF;
+        sendByte[3] = lspeed & 0xFF;
+        sendByte[4] = 0x02;
+        sendByte[5] = (rspeed >> 8) & 0xFF;
+        sendByte[6] = rspeed & 0xFF;
+        sendByte[7] = getcrc(sendByte, 7);
+        ser.write(sendByte, sizeof(sendByte));
+        cout << "sendByte: " << sendByte << endl;
+
         uint8_t bufferArray[28];
         serial_read(ser, result);
         if(result.length()==28){
@@ -103,20 +129,21 @@ int main(int argc, char** argv)
             sensor3 = ((bufferArray[23] << 8) & 0xFF) | (bufferArray[24] & 0xFF);
             sensor4 = ((bufferArray[25] << 8) & 0xFF) | (bufferArray[26] & 0xFF);
         }
-        cout << "lID: " << " " << lID << endl;
-        cout << "rID: " << " " << rID << endl;
-        cout << "Lspeed: " << " " << Lspeed << endl;
-        cout << "Rspeed: " << " " << Rspeed << endl;
-        cout << "accel_x: " << " " << accel_x << endl;
-        cout << "accel_y: " << " " << accel_y << endl;
-        cout << "accel_z: " << " " << accel_z << endl;
-        cout << "gyro_x: " << " " << gyro_x << endl;
-        cout << "gyro_y: " << " " << gyro_y << endl;
-        cout << "gyro_z: " << " " << gyro_z << endl;
-        cout << "sensor1: " << " " << sensor1 << endl;
-        cout << "sensor2: " << " " << sensor2 << endl;
-        cout << "sensor3: " << " " << sensor3 << endl;
-        cout << "sensor4: " << " " << sensor4 << endl;
+        // Output the result
+        // cout << "lID: " << " " << lID << endl;
+        // cout << "rID: " << " " << rID << endl;
+        // cout << "Lspeed: " << " " << Lspeed << endl;
+        // cout << "Rspeed: " << " " << Rspeed << endl;
+        // cout << "accel_x: " << " " << accel_x << endl;
+        // cout << "accel_y: " << " " << accel_y << endl;
+        // cout << "accel_z: " << " " << accel_z << endl;
+        // cout << "gyro_x: " << " " << gyro_x << endl;
+        // cout << "gyro_y: " << " " << gyro_y << endl;
+        // cout << "gyro_z: " << " " << gyro_z << endl;
+        // cout << "sensor1: " << " " << sensor1 << endl;
+        // cout << "sensor2: " << " " << sensor2 << endl;
+        // cout << "sensor3: " << " " << sensor3 << endl;
+        // cout << "sensor4: " << " " << sensor4 << endl;
     }
 
 	ser.close();
