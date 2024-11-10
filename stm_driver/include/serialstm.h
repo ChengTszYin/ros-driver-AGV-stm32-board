26958f099730e660b85712e5d5811123137f93af
ChengTszYin <ken1999429@gmail.com>
2024-11-10 20:59:19 +0800

    update2

--- stm_driver/include/serialstm.h
+++ stm_driver/include/serialstm.h
@@ -55,6 +55,13 @@ struct recvMessage
     uint8_t d80nk4;
 };
 
+struct velocity
+{
+    double linear_x;
+    double linear_y;
+    double angular_z;
+};
+
 class SerialSTM {
     private:
         string port = "COM4";
@@ -74,12 +81,13 @@ class SerialSTM {
         double y_pos = 0.0;
         double th = 0.0;
         ros::Time last_vel_time_;
-        // geometry_msgs::Vector3Stamped speed_msgs;
+        geometry_msgs::Vector3Stamped veloc_msgs;
         stm_driver::Wheel speed_msgs;
         sensor_msgs::Range front_dist;
         sensor_msgs::Range back_dist;
         sensor_msgs::Imu imu_msgs;
         std_msgs::Int8MultiArray wsad;
+        ros::Publisher vel_pub;
         ros::Publisher  ser_pub;
         ros::Publisher  front_pub;
         ros::Publisher  back_pub;
@@ -98,6 +106,8 @@ class SerialSTM {
         void bumpPublish(recvMessage* recvmsg);
         int notopen(std::string &result);
         uint8_t getcrc(uint8_t* Bytecode, int len);
+        velocity vel;
+        velocity getVelocity(double rpm1, double rpm2, double rpm3, double rpm4);
 };
 
 
